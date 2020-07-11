ac6c65cf9a3d660915cdcc5febfc585505a3e206
Joel Mateu <joel.mateu@enti.cat>
2020-07-11 23:11:05 +0200

    collisions and rgbd

---
+++ GameJam_Sq(2)/Assets/MoveOnDir.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnDir : MonoBehaviour
+{
+    //public Vector3 direction;
+    public Vector3 position;
+
+    //private Rigidbody body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //body = GetComponent<Rigidbody>();
+        position = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //body.MovePosition(transform.position +  direction * Time.deltaTime);
+        transform.position = position;
+    }
+}
